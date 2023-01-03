10e33b7f00cc7ecc46d6998d14f960d08dcee564
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-01-03 13:39:59 -0800

    initial commit

--- main/utils.py
+++ main/utils.py
@@ -13,11 +13,11 @@ from email.message import EmailMessage
 EMAIL_ADDRESS = "fashinaoluwaseun36@gmail.com"
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-def send_now(client_name, image_url):
+def send_now(client_name, image_url, user_email):
     msg = EmailMessage()
     msg['Subject'] = 'AMSB TERMS & CONDITIONS'
     msg['From'] = EMAIL_ADDRESS
-    msg['To'] = ['contact.seunfashina@gmail.com', 'akoredebakare4u@gmail.com']
+    msg['To'] = [user_email, 'amsbconnectsltd@outlook.com']
     msg.set_content('Hello, thanks for agreeing to our terms. Below is a copy of your agreement form. Have a nice day!')
 
     
@@ -46,11 +46,13 @@ def send_now(client_name, image_url):
         smtp.send_message(msg)
 
 
-def send_apply_form(payload):
+def send_apply_form(payload, user_email):
     msg = EmailMessage()
     msg['Subject'] = 'AMSB APPLICATION'
     msg['From'] = EMAIL_ADDRESS
-    msg['To'] = ['contact.seunfashina@gmail.com']
+    
+    msg['To'] = [user_email, 'amsbconnectsltd@outlook.com']
+    print(user_email)
     msg.set_content('Hello, here is a copy of our application form')
 
     
@@ -59,7 +61,30 @@ def send_apply_form(payload):
     
     rendered = render_to_string("emails/apply.html", context={
         "first_name" : payload.get('first_name'),
+        "middle_name" : payload.get('middle_name'),
         "last_name" : payload.get('last_name'),
+        "dob" : payload.get('dob'),
+        "gender" : payload.get('gender'),
+        "relationship" : payload.get('relationship'),
+        "employment" : payload.get('employment'),
+        "adress" : payload.get('adress'),
+        "citizenship" : payload.get('citizenship'),
+        "email" : payload.get('email'),
+        "tel" : payload.get('tel'),
+        "high_school" : payload.get('high_school'),
+        "post_secondary_sch" : payload.get('post_secondary_sch'),
+        "work" : payload.get('work'),
+        "travel" : payload.get('travel'),
+        "illness" : payload.get('illness'),
+        "accomodation" : payload.get('accomodation'),
+        "native_lang" : payload.get('native_lang'),
+        "other_lang" : payload.get('other_lang'),
+        "passport_num" : payload.get('passport_num'),
+        "issue_date_passport" : payload.get('issue_date_passport'),
+        "expire_date_passport" : payload.get('expire_date_passport'),
+        "visa_refusal" : payload.get('visa_refusal'),
+        "visa_refusal_reason" : payload.get('visa_refusal_reason'),
+
     })
 
 
--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import RegisterForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -37,13 +38,19 @@ class Login(LoginView):
 
 class Login(LoginView):
     template_name = 'authentication/login.html'
+
+@login_required(login_url = '/login')    
 def agreement(request):
+    username = request.user.username
+    user = User.objects.get(username=username)
+    user_email = user.email
+
     if request.method == 'POST':
         client_name = request.POST.get('client_name')
         image_url = request.POST.get('image_url')
 
         if client_name != "":
-            send_now(client_name=client_name, image_url=image_url)
+            send_now(client_name=client_name, image_url=image_url, user_email=user_email)
 
         else:
             return redirect('agreement')
@@ -54,22 +61,77 @@ def agreement(request):
 
 @login_required(login_url = '/login')
 def form(request):
-
+    username = request.user.username
+    user = User.objects.get(username=username)
+    user_email = user.email
+    print(user_email)
+    
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
+        middle_name = request.POST.get('middle_name')
         last_name = request.POST.get('last_name')
+        dob = request.POST.get('dob')
+        gender = request.POST.get('gender')
+        print(gender)
+        relationship = request.POST.get('relationship')
+        employment = request.POST.get('employment')
+        adress = request.POST.get('address')
+        citizenship = request.POST.get('citizenship')
+        email = request.POST.get('email')
+        tel = request.POST.get('tel')
+        high_school = request.POST.get('high_school')
+        post_secondary_sch = request.POST.get('post_secondary_sch')
+        work = request.POST.get('work')
+        travel = request.POST.get('travel')
+        travel_place = request.POST.get('travel_place')
+        illness = request.POST.get('illness')
+        accomodation = request.POST.get('accomodation')
+        native_lang = request.POST.get('native_lang')
+        other_lang = request.POST.get('other_lang')
+        passport_num = request.POST.get('passport_num')
+        issue_date_passport = request.POST.get('issue_date_passport')
+        expire_date_passport = request.POST.get('expire_date_passport')
+        visa_refusal = request.POST.get('visa_refusal')
+        visa_refusal_reason = request.POST.get('visa_refusal_reason')
+
+
 
     
         context = {
             'first_name' : first_name,
+            'middle_name' : middle_name,
             'last_name' : last_name, 
+            'dob': dob,
+            'gender': gender,
+            'relationship':relationship,
+            'employment' : employment,
+            'adress': adress,
+            'citizenship': citizenship,
+            'email' : email,
+            'tel': tel,
+            'high_school': high_school,
+            'post_secondary_sch': post_secondary_sch,
+            'work': work,
+            'travel': travel,
+            'travel_place':travel_place,
+            'illness':illness,
+            'accomodation':accomodation,
+            'native_lang':native_lang,
+            'other_lang':other_lang,
+            'passport_num':passport_num,
+            'issue_date_passport':issue_date_passport,
+            'expire_date_passport':expire_date_passport,
+            'visa_refusal':visa_refusal,
+            'visa_refusal_reason':visa_refusal_reason
+
         }
 
-        send_apply_form(payload=context)
+        send_apply_form(payload=context, user_email=user_email)
 
     return render(request, 'forms/form.html')
 
 def signup(request):
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
